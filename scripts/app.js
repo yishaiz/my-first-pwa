38f5a7b1f416808c04863f36e5aa1d7d11786f97
yishai <yishai5555@gmail.com>
2017-09-07 13:59:52 +0300

    '05-temp-worker'

--- scripts/app.js
+++ scripts/app.js
@@ -20,11 +20,11 @@
         isLoading: true,
         visibleCards: {},
         selectedCities: [],
-        spinner: document.querySelector ('.loader'),
-        cardTemplate: document.querySelector ('.cardTemplate'),
-        container: document.querySelector ('.main'),
-        addDialog: document.querySelector ('.dialog-container'),
-        daysOfWeek: [ 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun' ]
+        spinner: document.querySelector('.loader'),
+        cardTemplate: document.querySelector('.cardTemplate'),
+        container: document.querySelector('.main'),
+        addDialog: document.querySelector('.dialog-container'),
+        daysOfWeek: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
     };
 
 
@@ -34,39 +34,39 @@
      *
      ****************************************************************************/
 
-    document.getElementById ('butRefresh').addEventListener ('click', function () {
+    document.getElementById('butRefresh').addEventListener('click', function () {
         // Refresh all of the forecasts
-        app.updateForecasts ();
+        app.updateForecasts();
     });
 
-    document.getElementById ('butAdd').addEventListener ('click', function () {
+    document.getElementById('butAdd').addEventListener('click', function () {
         // Open/show the add new city dialog
-        app.toggleAddDialog (true);
+        app.toggleAddDialog(true);
     });
 
-    document.getElementById ('butAddCity').addEventListener ('click', function () {
+    document.getElementById('butAddCity').addEventListener('click', function () {
         // Add the newly selected city
-        var select = document.getElementById ('selectCityToAdd');
-        var selected = select.options[ select.selectedIndex ];
-        var key = selected.value;
-        var label = selected.textContent;
+        var select   = document.getElementById('selectCityToAdd');
+        var selected = select.options[select.selectedIndex];
+        var key      = selected.value;
+        var label    = selected.textContent;
 
         if (!app.selectedCities) {
             app.selectedCities = [];
         }
 
-        app.getForecast (key, label);
+        app.getForecast(key, label);
 
-        app.selectedCities.push ({ key: key, label: label });
+        app.selectedCities.push({key: key, label: label});
 
-        app.saveSelectedCities ();
+        app.saveSelectedCities();
 
-        app.toggleAddDialog (false);
+        app.toggleAddDialog(false);
     });
 
-    document.getElementById ('butAddCancel').addEventListener ('click', function () {
+    document.getElementById('butAddCancel').addEventListener('click', function () {
         // Close the add new city dialog
-        app.toggleAddDialog (false);
+        app.toggleAddDialog(false);
     });
 
 
@@ -79,77 +79,77 @@
     // Toggles the visibility of the add new city dialog.
     app.toggleAddDialog = function (visible) {
         if (visible) {
-            app.addDialog.classList.add ('dialog-container--visible');
+            app.addDialog.classList.add('dialog-container--visible');
         } else {
-            app.addDialog.classList.remove ('dialog-container--visible');
+            app.addDialog.classList.remove('dialog-container--visible');
         }
     };
 
     // Updates a weather card with the latest weather forecast. If the card
     // doesn't already exist, it's cloned from the template.
     app.updateForecastCard = function (data) {
-        var dataLastUpdated = new Date (data.created);
-        var sunrise = data.channel.astronomy.sunrise;
-        var sunset = data.channel.astronomy.sunset;
-        var current = data.channel.item.condition;
-        var humidity = data.channel.atmosphere.humidity;
-        var wind = data.channel.wind;
+        var dataLastUpdated = new Date(data.created);
+        var sunrise         = data.channel.astronomy.sunrise;
+        var sunset          = data.channel.astronomy.sunset;
+        var current         = data.channel.item.condition;
+        var humidity        = data.channel.atmosphere.humidity;
+        var wind            = data.channel.wind;
 
-        var card = app.visibleCards[ data.key ];
+        var card = app.visibleCards[data.key];
         if (!card) {
-            card = app.cardTemplate.cloneNode (true);
-            card.classList.remove ('cardTemplate');
-            card.querySelector ('.location').textContent = data.label;
-            card.removeAttribute ('hidden');
-            app.container.appendChild (card);
-            app.visibleCards[ data.key ] = card;
+            card = app.cardTemplate.cloneNode(true);
+            card.classList.remove('cardTemplate');
+            card.querySelector('.location').textContent = data.label;
+            card.removeAttribute('hidden');
+            app.container.appendChild(card);
+            app.visibleCards[data.key] = card;
         }
 
         // Verifies the data provide is newer than what's already visible
         // on the card, if it's not bail, if it is, continue and update the
         // time saved in the card
-        var cardLastUpdatedElem = card.querySelector ('.card-last-updated');
-        var cardLastUpdated = cardLastUpdatedElem.textContent;
+        var cardLastUpdatedElem = card.querySelector('.card-last-updated');
+        var cardLastUpdated     = cardLastUpdatedElem.textContent;
         if (cardLastUpdated) {
-            cardLastUpdated = new Date (cardLastUpdated);
+            cardLastUpdated = new Date(cardLastUpdated);
             // Bail if the card has more recent data then the data
-            if (dataLastUpdated.getTime () < cardLastUpdated.getTime ()) {
+            if (dataLastUpdated.getTime() < cardLastUpdated.getTime()) {
                 return;
             }
         }
         cardLastUpdatedElem.textContent = data.created;
 
-        card.querySelector ('.description').textContent = current.text;
-        card.querySelector ('.date').textContent = current.date;
-        card.querySelector ('.current .icon').classList.add (app.getIconClass (current.code));
-        card.querySelector ('.current .temperature .value').textContent =
-            Math.round (current.temp);
-        card.querySelector ('.current .sunrise').textContent = sunrise;
-        card.querySelector ('.current .sunset').textContent = sunset;
-        card.querySelector ('.current .humidity').textContent =
-            Math.round (humidity) + '%';
-        card.querySelector ('.current .wind .value').textContent =
-            Math.round (wind.speed);
-        card.querySelector ('.current .wind .direction').textContent = wind.direction;
-        var nextDays = card.querySelectorAll ('.future .oneday');
-        var today = new Date ();
-        today = today.getDay ();
+        card.querySelector('.description').textContent = current.text;
+        card.querySelector('.date').textContent        = current.date;
+        card.querySelector('.current .icon').classList.add(app.getIconClass(current.code));
+        card.querySelector('.current .temperature .value').textContent =
+            Math.round(current.temp);
+        card.querySelector('.current .sunrise').textContent            = sunrise;
+        card.querySelector('.current .sunset').textContent             = sunset;
+        card.querySelector('.current .humidity').textContent           =
+            Math.round(humidity) + '%';
+        card.querySelector('.current .wind .value').textContent        =
+            Math.round(wind.speed);
+        card.querySelector('.current .wind .direction').textContent    = wind.direction;
+        var nextDays                                                   = card.querySelectorAll('.future .oneday');
+        var today                                                      = new Date();
+        today                                                          = today.getDay();
         for (var i = 0; i < 7; i++) {
-            var nextDay = nextDays[ i ];
-            var daily = data.channel.item.forecast[ i ];
+            var nextDay = nextDays[i];
+            var daily   = data.channel.item.forecast[i];
             if (daily && nextDay) {
-                nextDay.querySelector ('.date').textContent =
-                    app.daysOfWeek[ (i + today) % 7 ];
-                nextDay.querySelector ('.icon').classList.add (app.getIconClass (daily.code));
-                nextDay.querySelector ('.temp-high .value').textContent =
-                    Math.round (daily.high);
-                nextDay.querySelector ('.temp-low .value').textContent =
-                    Math.round (daily.low);
+                nextDay.querySelector('.date').textContent =
+                    app.daysOfWeek[(i + today) % 7];
+                nextDay.querySelector('.icon').classList.add(app.getIconClass(daily.code));
+                nextDay.querySelector('.temp-high .value').textContent =
+                    Math.round(daily.high);
+                nextDay.querySelector('.temp-low .value').textContent  =
+                    Math.round(daily.low);
             }
         }
         if (app.isLoading) {
-            app.spinner.setAttribute ('hidden', true);
-            app.container.removeAttribute ('hidden');
+            app.spinner.setAttribute('hidden', true);
+            app.container.removeAttribute('hidden');
             app.isLoading = false;
         }
     };
@@ -171,36 +171,36 @@
      */
     app.getForecast = function (key, label) {
         var statement = 'select * from weather.forecast where woeid=' + key;
-        var url = 'https://query.yahooapis.com/v1/public/yql?format=json&q=' +
+        var url       = 'https://query.yahooapis.com/v1/public/yql?format=json&q=' +
             statement;
         // TODO add cache logic here
 
         // Fetch the latest data.
-        var request = new XMLHttpRequest ();
+        var request                = new XMLHttpRequest();
         request.onreadystatechange = function () {
             if (request.readyState === XMLHttpRequest.DONE) {
                 if (request.status === 200) {
-                    var response = JSON.parse (request.response);
-                    var results = response.query.results;
-                    results.key = key;
-                    results.label = label;
+                    var response    = JSON.parse(request.response);
+                    var results     = response.query.results;
+                    results.key     = key;
+                    results.label   = label;
                     results.created = response.query.created;
-                    app.updateForecastCard (results);
+                    app.updateForecastCard(results);
                 }
             } else {
                 // Return the initial weather forecast since no data is available.
-                app.updateForecastCard (initialWeatherForecast);
+                app.updateForecastCard(initialWeatherForecast);
             }
         };
-        request.open ('GET', url);
-        request.send ();
+        request.open('GET', url);
+        request.send();
     };
 
     // Iterate all of the cards and attempt to get the latest forecast data
     app.updateForecasts = function () {
-        var keys = Object.keys (app.visibleCards);
-        keys.forEach (function (key) {
-            app.getForecast (key);
+        var keys = Object.keys(app.visibleCards);
+        keys.forEach(function (key) {
+            app.getForecast(key);
         });
     };
 
@@ -208,13 +208,13 @@
 
     // Save list of cities to localStorage.
     app.saveSelectedCities = function () {
-        var selectedCities = JSON.stringify (app.selectedCities);
+        var selectedCities          = JSON.stringify(app.selectedCities);
         localStorage.selectedCities = selectedCities;
     };
 
     app.getIconClass = function (weatherCode) {
         // Weather codes: https://developer.yahoo.com/weather/documentation.html#codes
-        weatherCode = parseInt (weatherCode);
+        weatherCode = parseInt(weatherCode);
         switch (weatherCode) {
             case 25: // cold
             case 32: // sunny
@@ -298,13 +298,13 @@
                     code: 24
                 },
                 forecast: [
-                    { code: 44, high: 86, low: 70 },
-                    { code: 44, high: 94, low: 73 },
-                    { code: 4, high: 95, low: 78 },
-                    { code: 24, high: 75, low: 89 },
-                    { code: 24, high: 89, low: 77 },
-                    { code: 44, high: 92, low: 79 },
-                    { code: 44, high: 89, low: 77 }
+                    {code: 44, high: 86, low: 70},
+                    {code: 44, high: 94, low: 73},
+                    {code: 4, high: 95, low: 78},
+                    {code: 24, high: 75, low: 89},
+                    {code: 24, high: 89, low: 77},
+                    {code: 44, high: 92, low: 79},
+                    {code: 44, high: 89, low: 77}
                 ]
             },
             atmosphere: {
@@ -323,10 +323,10 @@
     app.selectedCities = localStorage.selectedCities;
 
     if (app.selectedCities) {
-        app.selectedCities = JSON.parse (app.selectedCities);
+        app.selectedCities = JSON.parse(app.selectedCities);
 
-        app.selectedCities.forEach (function (city) {
-            app.getForecast (city.key, city.label);
+        app.selectedCities.forEach(function (city) {
+            app.getForecast(city.key, city.label);
         });
     }
     else {
@@ -336,15 +336,51 @@
          * that data into the page.
          */
 
-        app.updateForecastCard (initialWeatherForecast);
+        app.updateForecastCard(initialWeatherForecast);
 
         app.selectedCities = [
-            { key: initialWeatherForecast.key, label: initialWeatherForecast.label }
+            {key: initialWeatherForecast.key, label: initialWeatherForecast.label}
         ];
 
-        app.saveSelectedCities ();
+        app.saveSelectedCities();
     }
 
 
     // TODO add service worker code here
-}) ();
+
+
+    //service worker sandbox
+    if (navigator.serviceWorker) {
+        navigator.serviceWorker.register('./sw.js', {scope: './about'})
+            .then(function (registration) {
+                console.log(registration);
+            })
+            .catch(function (e) {
+                console.error(e);
+            })
+    }
+    else {
+        console.log('Service Worker is not supported in this browser.');
+    }
+
+
+    /*
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function () {
+
+            navigator.serviceWorker.register('/scripts/sw.js')
+                .then(function (registration) {
+
+                        // Registration was successful
+                        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+                    },
+                    function (err) {
+                        // registration failed :(
+                        console.log('ServiceWorker registration failed: ', err);
+                    });
+        });
+    }
+*/
+
+
+})();
